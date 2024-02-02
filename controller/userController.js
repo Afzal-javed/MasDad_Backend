99e7d3711b504fb85d60682c8cf9a592ca0e86db
afzal javed <afzaljaved59832@gmail.com>
2024-02-02 17:05:11 +0530

    authentication added to backend

--- controller/userController.js
+++ controller/userController.js
@@ -2,9 +2,14 @@ import Users from "../models/userModel.js"
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import OTP from "../models/otpModel.js";
+import nodemailer from "nodemailer";
+
 dotenv.config();
+
+
 export const register = async (req, res) => {
-    const { fullName, email, password } = req.body
+    const { fullName, email, password, uid } = req.body
     try {
         const isUserAlreadyExist = await Users.findOne({ email });
         if (isUserAlreadyExist) {
@@ -15,10 +20,12 @@ export const register = async (req, res) => {
             const newUser = new Users({
                 fullName,
                 email,
-                password: hashedPassword
+                password: hashedPassword,
+                uid
             })
             await newUser.save();
             res.status(200).send("User register successfully");
+            sendGreetingEmail(email, fullName);
         }
     } catch (error) {
         res.status(500).send("Internal server error")
@@ -54,9 +61,9 @@ export const login = async (req, res) => {
     }
 }
 export const logout = async (req, res) => {
-    const id = req.params.userId;
+    const id = req.params.id;
     try {
-        const user = await Users.findOne(id)
+        const user = await Users.findById(id);
         if (!user) {
             res.status(404).send("User does not exist")
         } else {
@@ -68,3 +75,130 @@ export const logout = async (req, res) => {
         res.status(500).send("Internal server error");
     }
 }
+
+export const generateOTP = async (req, res) => {
+    const { email } = req.body;
+    try {
+        const userNotExist = await Users.findOne({ email });
+        if (!userNotExist) {
+            res.status(404).send("User Does Not Exist");
+        } else {
+            let otp = Math.floor((Math.random() * 1000000) + 1)
+            let otpData = new OTP({
+                email: email,
+                otpCode: otp,
+                expireIn: new Date().getTime() + 120 * 1000
+            })
+            await otpData.save();
+            res.status(200).send("OTP sent successfully to your email");
+            sendOTPEmail(email, otp);
+        }
+    } catch (error) {
+        res.status(500).send("Internal server error")
+    }
+}
+export const verifyOtp = async (req, res) => {
+    try {
+        const { email, otp } = req.body;
+        const data = await OTP.findOne({ email: email, otpCode: otp })
+        if (!data) {
+            res.status(400).send("Enter the valid OTP");
+        } else {
+            let currTime = new Date().getTime();
+            let diff = data.expireIn - currTime;
+            if (diff >= 0) {
+                res.status(200).send("OTP has been verified")
+            } else {
+                res.status(400).send("OTP has been expire")
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+export const confirmPassword = async (req, res) => {
+    try {
+        const { email, newPassword } = req.body;
+        const user = await Users.findOne({ email });
+        if (!user) {
+            res.status(404).send("User Not Register")
+        } else {
+            const salt = bcrypt.genSaltSync(10)
+            const hashedPassword = bcrypt.hashSync(newPassword, salt);
+            user.password = hashedPassword;
+            await user.save();
+            res.status(200).send("Password updated successfully You can login Now");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+const sendGreetingEmail = async (email, name) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASS
+            },
+        })
+        const GreetingMail = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: `Welcome to MAS-DAD`,
+            text: `Dear ${name},
+            Thank you for registering with MAS-DAD. We are excited to have you on board!.`
+        }
+        const result = await transporter.sendMail(GreetingMail);
+        return result;
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw error;
+    }
+}
+
+const sendOTPEmail = async (email, otp) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'afzaljaved59832@gmail.com',
+                pass: 'dgmfeoroabxahwuj',
+            },
+        });
+        const mailOptions = {
+            from: 'afzaljaved59832@gmail.com',
+            to: email,
+            subject: 'OTP for Verification',
+            text: `Your OTP is ${otp}. Please use it to verify your account.`,
+        };
+        const result = await transporter.sendMail(mailOptions);
+        return result;
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw error;
+    }
+};
+// export const logout = async (req, res) => {
+//     const id = req.params.userId;
+
+//     try {
+//         const user = await Users.findOne({ _id: ObjectId(id) });
+//         console.log(user);
+
+//         if (!user) {
+//             res.status(404).send("User does not exist");
+//         } else {
+//             user.token = '';
+//             await user.save();
+//             res.status(200).send("User Logout successfully");
+//         }
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("Internal server error");
+//     }
+// }
